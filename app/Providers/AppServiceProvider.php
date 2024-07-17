e79492b3b4ab1df134c392ca6cccd94db910f794
Joyce <787027175@qq.com>
2024-07-17 17:20:12 +0800

    feat: 更新到 laravel 11

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Services\Tenancy;
 use Illuminate\Support\ServiceProvider;
-use Module\Tenant\Models\Tenant\TenantModel;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,20 +12,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
-        $this->app->singleton(Tenancy::class);
-
-        $this->app->bind(TenantModel::class, function ($app) {
-            return $app[Tenancy::class]->tenant;
-        });
     }
 
     /**
      * Bootstrap any application services.
-     * @return void
-     * @throws \ReflectionException
      */
     public function boot(): void
     {
+        //
     }
-
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,16 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call([
-            SysMenuSeeder::class,
-            SysRegionSeeder::class,
-            TenantSeeder::class
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
