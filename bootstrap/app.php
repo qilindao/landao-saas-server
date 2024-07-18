76ac9a1d15e7025c69f0d4137824c192b99d5a51
Joyce <787027175@qq.com>
2024-07-18 17:21:42 +0800

    feat(bootstrap):去掉路由文件引导，改成原生注解路由；引入自定义中间件

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,22 @@
 <?php
 
+use App\Http\Middleware\UserOperateLog;
+use App\Http\Middleware\TenantPermission;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'userOperate.log' => UserOperateLog::class,//用户审计日记
+            'auth.tenant' => TenantPermission::class,//租户权限
+            'api.case.converter'=>\LanDao\LaravelCore\Middleware\ApiCaseConverter::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
