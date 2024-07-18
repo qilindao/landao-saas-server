0710938a448156fd2cd41b5202933917da00ac38
Joyce <787027175@qq.com>
2024-07-18 17:27:20 +0800

    feat(helper):通用助手函数

---
+++ app/helpers.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use App\Services\Tenancy;
+use Module\Tenant\Models\Tenant\TenantModel;
+
+if (!function_exists('tenancy')) {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed
+     */
+    function tenancy()
+    {
+        return app(Tenancy::class);
+    }
+}
+
+if (!function_exists('tenant')) {
+    /**
+     * Get a key from the current tenant's storage.
+     *
+     */
+    /**
+     * @param $key
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed|void|null
+     */
+    function tenant($key = null)
+    {
+        if (!app()->bound(TenantModel::class)) {
+            return;
+        }
+        if (is_null($key)) {
+            return app(TenantModel::class);
+        }
+
+        return optional(app(TenantModel::class))->getAttribute($key) ?? null;
+    }
+}
