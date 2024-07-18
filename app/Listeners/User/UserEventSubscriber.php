bf4c52cff9dd6353d82a26fb8cb4025bed9b21d6
Joyce <787027175@qq.com>
2024-07-18 17:26:08 +0800

    feat(event):用户登陆事件

---
+++ app/Listeners/User/UserEventSubscriber.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Listeners\User;
+
+use App\Events\User\UserLoginEvent;
+use App\Events\User\UserLoginLogEvent;
+use Illuminate\Events\Dispatcher;
+use LanDao\LaravelCore\Helpers\StrHelper;
+use Module\Tenant\Repositories\User\LoginLogRepo;
+
+/**
+ * 用户登陆登出订阅者
+ */
+class UserEventSubscriber
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * 登录成功，更新用户信息
+     * Handle the event.
+     */
+    public function handleLogin(UserLoginEvent $event): void
+    {
+        $clientIp = StrHelper::ip2long(request()->ip());
+        $nowTime = now()->timestamp;
+        $user = $event->userModel;
+        $user->update([
+            'refresh_time' => $nowTime,
+            'refresh_ip' => $clientIp,
+            'last_login_ip' => $clientIp,
+            'updated_at' => $nowTime,
+            'last_login_time' => $nowTime
+        ]);
+    }
+
+    /**
+     * 登录之后，记录用户登录日志
+     * @param UserLoginLogEvent $event
+     * @return void
+     */
+    public function handleLoginLog(UserLoginLogEvent $event): void
+    {
+        $user = $event->userModel;
+        $loginLogRepo = app(LoginLogRepo::class);
+        $loginLogRepo->create([
+            'user_id' => $user->user_id,
+            'username' => $user->username,
+            'user_ip' => request()->ip(),
+            'user_agent' => request()->server('HTTP_USER_AGENT'),
+            'from_app' => $event->fromApp,
+            'result' => $event->msg,
+            'tenant_id' => $user->tenant_id
+        ]);
+    }
+
+    /**
+     * 为订阅者注册侦听器。
+     * @param Dispatcher $events
+     * @return void
+     */
+    public function subscribe(Dispatcher $events): void
+    {
+        $events->listen(UserLoginEvent::class, [
+            UserEventSubscriber::class, 'handleLogin'
+        ]);
+
+        $events->listen(UserLoginLogEvent::class, [
+            UserEventSubscriber::class, 'handleLoginLog'
+        ]);
+    }
+}
